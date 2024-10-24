9d159548af78d2161964706b9e8bdfbb4de286a8
Aldrazus <aespinal59@gmail.com>
2024-10-24 11:52:35 -0400

    update telescope

---
+++ nvim/lua/config/lspconfig.lua
@@ -0,0 +1,12 @@
+local map = vim.keymap.set
+
+local M = {}
+
+M.on_attach = function(_, bufnr)
+
+end
+return {
+  on_attach = function(_, bufnr)
+
+  end,
+}
--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -25,4 +25,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+vim.keymap.set('n', '<leader>ff', "<cmd>Telescope find_files<CR>", { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', "<cmd>Telescope live_grep<CR>", { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<CR>", { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', "<cmd>Telescope help_tags<CR>", { desc = 'Telescope help tags' })
+
 -- TODO: add something for creating new files more quickly
