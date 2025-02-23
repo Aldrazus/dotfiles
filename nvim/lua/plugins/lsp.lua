1f5bb6c1af1497fba246a17457a9345311e7341e
Aldrazus <aespinal59@gmail.com>
2025-02-23 11:37:55 -0500

    dont cmp in command mode

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -56,15 +56,21 @@ return {
         "saghen/blink.cmp",
         dependencies = "rafamadriz/friendly-snippets",
         version = "*",
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
         opts = {
-            keymap = { preset = "default" },
+            keymap = { preset = "enter" },
             appearance = {
                 use_nvim_cmp_as_default = true,
                 nerd_font_variant = "mono"
             },
+            cmdline = {
+                enabled = false
+            },
             sources = {
                 default = { "lsp", "path", "snippets", "buffer"}
             },
+            signature = { enabled = true }
         },
 
         opts_extend = { "sources.default" }
