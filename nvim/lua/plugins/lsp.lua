e220797c106e25561ff8a56b252d59ab53c9c362
Aldrazus <aespinal59@gmail.com>
2025-03-31 17:58:10 -0400

    show hidden files

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -16,7 +16,7 @@ return {
         },
         config = function ()
             require("mason-lspconfig").setup({
-                ensure_installed = {"clangd", "lua_ls"}
+                ensure_installed = {"clangd", "lua_ls", "volar", "ts_ls"}
             })
             require("mason-lspconfig").setup_handlers({
                 function (server_name)
