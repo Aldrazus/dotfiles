5991dbbabd4472f34a69b194ce4ee65e2c2d56fd
Alberto Espinal <aespinal59@gmail.com>
2025-12-04 19:30:31 -0500

    install ocaml

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,12 @@ set -Ux EDITOR nvim
 set -Ux SUDO_EDITOR nvim
 set -Ux VISUAL nvim
 set -g fish_prompt_pwd_dir_length 0
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/aldrazus/.opam/opam-init/init.fish' && source '/home/aldrazus/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
