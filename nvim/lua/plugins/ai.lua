3a79cd8a1b2a2ebf05d4f4957e43e7194e9b04dc
Aldrazus <aespinal59@gmail.com>
2025-12-19 11:58:22 -0500

    configure codecompanion

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -9,4 +9,18 @@ return {
       vim.g.copilot_no_tab_map = true
     end
   },
+  {
+    "olimorris/codecompanion.nvim",
+    version = "^18.0.0",
+    opts = {},
+    keys = {
+      { "<C-a>", "<cmd>CodeCompanionActions<cr>", mode = {"n", "v"}, noremap = true, silent = true},
+      { "<Leader>a", "<cmd>CodeCompanionChat Toggle<cr>", mode = {"n", "v"}, noremap = true, silent = true},
+      { "ga", "<cmd>CodeCompanionChat Add<cr>", mode = {"v"}, noremap = true, silent = true}
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  }
 }
