79aaea9ef9560be8a89d06ece14051b34abb0363
Aldrazus <aespinal59@gmail.com>
2026-01-13 09:38:01 -0500

    change vim binding stuff

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -29,4 +29,5 @@ if vim.g.vscode then
     -- undo/REDO via vscode
     vim.keymap.set("n","u","<Cmd>call VSCodeNotify('undo')<CR>")
     vim.keymap.set("n","<C-r>","<Cmd>call VSCodeNotify('redo')<CR>") 
+    vim.keymap.set("n", "<C-b>", "<Cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<CR>")
 end
