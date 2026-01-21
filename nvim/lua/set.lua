0884ec8c1fdbafe7c9717498f571d3ea92543cf3
Aldrazus <aespinal59@gmail.com>
2026-01-21 10:23:41 -0500

    always show signcolumn

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -39,6 +39,7 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.completeopt = "menuone,noinsert,noselect"
+vim.opt.signcolumn = "yes"
 vim.opt.cmdheight = 2
 vim.opt.updatetime = 50
 vim.opt.mouse = "a"
