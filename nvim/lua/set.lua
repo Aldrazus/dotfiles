af0cb50f0d2465b272c39647d2cb1410da305011
Aldrazus <aespinal59@gmail.com>
2026-01-21 10:19:40 -0500

    fix signcol

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -39,7 +39,6 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.completeopt = "menuone,noinsert,noselect"
-vim.opt.signcolumn = "number"
 vim.opt.cmdheight = 2
 vim.opt.updatetime = 50
 vim.opt.mouse = "a"
